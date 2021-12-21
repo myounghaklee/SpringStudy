46efa947684e00b17c6837353043ab1f26fc0246
myounghak <leemh9207@ajou.ac.kr>
2021-12-22 00:06:50 +0900

    add role test

--- core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -3,6 +3,7 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextInfoTest {
@@ -17,10 +18,27 @@ public class ApplicationContextInfoTest {
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("beanDefinitionName  " + beanDefinitionName + "object ="+ bean);
 
-
-
         }
 
     }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition bd = ac.getBeanDefinition(beanDefinitionName);
+
+            //Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            //Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            if(bd.getRole() == bd.ROLE_INFRASTRUCTURE){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("bean = " + beanDefinitionName  + "object = " + bean);
+
+            }
+
+        }
+    }
+
 
 }
