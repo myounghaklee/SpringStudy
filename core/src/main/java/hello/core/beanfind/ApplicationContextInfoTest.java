490419b37c308f63a97aee4be54c1f9bd364cbfa
myounghak <leemh9207@ajou.ac.kr>
2021-12-21 23:56:13 +0900

    no message

--- core/src/main/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++
@@ -1,5 +0,0 @@
-package hello.core.beanfind;
-
-public class ApplicationContextInfoTest {
-    
-}
---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,26 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String [] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName  " + beanDefinitionName + "object ="+ bean);
+
+
+
+        }
+
+    }
+
+}
