faaa786806358aedf530c5f19daf9f7497d5fcb8
myounghak <leemh9207@ajou.ac.kr>
2021-12-24 17:51:44 +0900

    add SingleTon Code

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,6 +2,7 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,15 @@ public class SingletonTest {
         AppConfig ap = new AppConfig();
         //1. 조회 : 호출할때마다 객체를 생성
         MemberService memberService1 = ap.memberService();
+
+        //2. 조회 : 호출할 때 마다 다른객체를 생성
+        MemberService memberService2 = ap.memberService();
+
+        //참조값이 다른것 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
+
 }
