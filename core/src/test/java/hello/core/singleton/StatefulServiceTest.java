e2221f59b783dcd0cb0bb5f28b9cc3191fe7587a
myounghak <leemh9207@ajou.ac.kr>
2021-12-26 23:39:36 +0900

    stateful test - singleton단점

---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,39 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService st1 = ac.getBean(StatefulService.class);
+        StatefulService st2 = ac.getBean(StatefulService.class);
+
+        //ThreadA : A사용자가 10000원 주문
+        st1.order("userA", 10000);
+
+        //ThreadA : B사용자가 20000원 주문
+        st2.order("userB", 20000);
+
+        //TreadA: 사용자A 주문금핵 조회
+        int price = st1.getPrice();
+        System.out.println("price = " + price);
+        org.assertj.core.api.Assertions.assertThat(st1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+
+}
