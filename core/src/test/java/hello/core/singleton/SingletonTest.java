75be2839df6cd50aa0b35a496af874cc8356d885
myounghak <leemh9207@ajou.ac.kr>
2021-12-26 22:00:23 +0900

    add singleton test code

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,5 +44,24 @@ public class SingletonTest {
         //same ==  대상의 주소값을 비교 참조랑 비교
         //equal ==  대상의 내용을 비교
     }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        //AppConfig ap = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다른것 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+
 
 }
