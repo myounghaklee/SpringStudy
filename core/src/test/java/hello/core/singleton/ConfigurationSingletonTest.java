2bf793c01a3e2fc85befd8eae7dc728032c55e7e
myounghak <leemh9207@ajou.ac.kr>
2021-12-28 00:28:49 +0900

    add @ConfigurationScan code

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -33,4 +33,22 @@ public class ConfigurationSingletonTest {
 
 
     }
+
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = : " + bean.getClass());
+        /* 결과
+        call AppConfig.memberService
+        23:57:51.675 [Test worker] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Creating shared instance of singleton bean 'memberRepository'
+        call AppConfig.memberRepository
+        23:57:51.677 [Test worker] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Creating shared instance of singleton bean 'orderService'
+        call AppConfig.orderService
+        23:57:51.679 [Test worker] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Creating shared instance of singleton bean 'discountPolicy'
+        bean = : class hello.core.AppConfig$$EnhancerBySpringCGLIB$$6dbfdd22
+
+         */
+    }
 }
