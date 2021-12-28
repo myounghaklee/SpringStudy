9ce84f7288455cc03a30fd05c5d850c63ce677ec
myounghak <leemh9207@ajou.ac.kr>
2021-12-29 02:37:46 +0900

    basePackage 옵션 사용 x 설정파일을 최상단 위치 주석 추가

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+
+/*
+@ComponentScan(
+        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+basePackage를 안주면 소스의 패키지인 hello.core 하위를 전부 search한다.
+
+ */
 @Configuration
 @ComponentScan(
         basePackages = "hello.core.member",
