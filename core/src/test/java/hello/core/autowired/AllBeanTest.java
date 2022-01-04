42c406f09fb945743aaa8e454aca3c258c4e1be8
myounghak <leemh9207@ajou.ac.kr>
2022-01-05 00:00:08 +0900

    Map사용 add

--- core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -1,11 +1,48 @@
 package hello.core.autowired;
 
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class AllBeanTest {
     @Test
     void findAllBean(){
-        new AnnotationConfigApplicationContext();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member mb = new Member(1L, "userA", Grade.VIP);
+        int dscountPrice = discountService.discount(mb, 10000, "fixDiscountPolicy");
+
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(dscountPrice).isEqualTo(1000);
+    }
+
+    static class DiscountService{
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policymap : " + policyMap);
+            System.out.println("policies : " + policies);
+        }
+
+        public int discount(Member mb, int i, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(mb, i);
+        }
     }
 }
