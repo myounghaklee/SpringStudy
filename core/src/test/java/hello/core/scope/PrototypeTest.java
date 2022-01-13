86d88c05ccdf5349e852cc84f6e8344cb6ec2ce6
myounghak <leemh9207@ajou.ac.kr>
2022-01-13 23:56:21 +0900

    prototype bean test code

--- core/src/test/java/hello/core/scope/PrototypeTest.java
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -1,6 +1,5 @@
 package hello.core.scope;
 
-import net.minidev.json.JSONUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -10,21 +9,23 @@ import javax.annotation.PreDestroy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonTest {
+public class PrototypeTest {
 
     @Test
     void singletonBeanFind(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
-
+        System.out.println("find prototypeBean1");
         SingletonBean sb1 = ac.getBean(SingletonBean.class);
+
+        System.out.println("find prototypeBean2");
         SingletonBean sb2 = ac.getBean(SingletonBean.class);
-        System.out.println("ssss1: " + sb1);
-        System.out.println("ssss2: " + sb2);
-        assertThat(sb1).isSameAs(sb2);
+        System.out.println("prototypeBean1: " + sb1);
+        System.out.println("prototypeBean1: "+ sb2);
+        assertThat(sb1).isNotSameAs(sb2);
         ac.close();
     }
 
-    @Scope("singleton")
+    @Scope("prototype")
     static class SingletonBean{
         @PostConstruct
         public void init(){
