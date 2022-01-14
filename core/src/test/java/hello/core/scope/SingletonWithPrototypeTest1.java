9a9c0f2e39e01687be5236029d159d7183319f5a
myounghak <leemh9207@ajou.ac.kr>
2022-01-15 00:25:57 +0900

    싱글톤, 프로토타입 같이사용 테스트코드 추가

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -25,10 +26,38 @@ public class SingletonWithPrototypeTest1 {
 
     }
 
+    @Test
+    void singletonClientUsePrototype(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean cb1 = ac.getBean(ClientBean.class);
+        int cnt1= cb1.logic();
+        Assertions.assertThat(cnt1).isSameAs(1);
+        ClientBean cb2 = ac.getBean(ClientBean.class);
+        int cnt2= cb1.logic();
+        Assertions.assertThat(cnt2).isSameAs(2);
+    }
+    @Scope("singleton")
+    static class ClientBean{
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean){
+            this.prototypeBean = prototypeBean;
+        }
+        public int logic(){
+            prototypeBean.addCnt();
+            return prototypeBean.getCnt();
+        }
+    }
+
+
     @Scope("prototype")
     static class PrototypeBean{
         private int cnt = 0;
-        public void addCnt(){ cnt++; }
+        public void addCnt(){
+            cnt++;
+        }
 
         public int getCnt(){
             return cnt;
@@ -36,7 +65,7 @@ public class SingletonWithPrototypeTest1 {
 
         @PostConstruct
         public void init(){
-            System.out.println("cnt === : " + this);
+            System.out.println("cnt === : " + this + "  "+ cnt);
         }
 
         @PreDestroy
