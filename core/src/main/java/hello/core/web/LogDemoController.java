6e7042543005a052488a234cb47839e6e9c2c38b
myounghak <leemh9207@ajou.ac.kr>
2022-01-15 19:51:31 +0900

    MyLogger code add

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,8 +17,14 @@ public class LogDemoController {
     private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
-    @RequestBody
+    @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURl(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+
     }
 }
