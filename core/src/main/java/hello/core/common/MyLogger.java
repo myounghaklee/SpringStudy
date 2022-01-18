f30b16f99c63caeb8c6c1547a696c6ac8608f6e8
myounghak <leemh9207@ajou.ac.kr>
2022-01-18 23:17:50 +0900

    proxy사용

--- core/src/main/java/hello/core/common/MyLogger.java
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.context.annotation.Scope;
 
@@ -9,7 +10,7 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value ="request")
+@Scope(value ="request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
     private  String uuid;
     private String requestURl;
