f4189cea40a96bd71001a23f2e056f3076e894df
myounghak <leemh9207@ajou.ac.kr>
2022-01-23 15:06:23 +0900

    lombok 사용

--- jpashop/src/main/java/jpabook/jpashop/Hello.java
+++ jpashop/src/main/java/jpabook/jpashop/Hello.java
@@ -1,4 +1,9 @@
 package jpabook.jpashop;
 
+import lombok.Data;
+
+@Data
 public class Hello {
+
+    private String data;
 }
--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+		Hello h = new Hello();
+		h.setData("hello");
+		String data = h.getData();
+		System.out.println(data +"------");
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
