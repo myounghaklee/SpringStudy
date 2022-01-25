a8b198cd0ba9b5c318cc9510a71f01105a171e87
myounghak <leemh9207@ajou.ac.kr>
2022-01-25 23:40:46 +0900

    rollback(false) annotation추가

--- jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -11,4 +11,13 @@ import javax.persistence.PersistenceContext;
 public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
 }
