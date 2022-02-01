fe56170affbc0f97344f79eb432672ceb829e41b
myounghak <leemh9207@ajou.ac.kr>
2022-02-02 01:47:43 +0900

    add Entities

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,4 +1,29 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter@Setter
 public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    private Item item;
+
+    private Order order;
+
+    private int orderPrice; //주문 가격
+
+    private int count;//주문 수량
+
+
+
 }
