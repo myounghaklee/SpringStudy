1a558fd61fd98439df1aef47a5d799ccb66317ec
myounghak <leemh9207@ajou.ac.kr>
2022-02-02 00:07:53 +0900

    add Entities

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,4 +1,15 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+@Embeddable
+@Getter
 public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,4 +1,35 @@
 package jpabook.jpashop.domain;
 
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
 public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "memeber_id")
+    private Member member;
+
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; //주문시간
+
+    private OrderStatus status ; //주문상태[Order, Cancle]
+
+
 }
