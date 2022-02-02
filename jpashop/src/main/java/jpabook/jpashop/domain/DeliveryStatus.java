043ea7593f92f2342faf8a08569cf1913e22bef6
myounghak <leemh9207@ajou.ac.kr>
2022-02-02 21:19:43 +0900

    Entity 작성중

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/DeliveryStatus.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class DeliveryStatus {
+}
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderStatus.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderStatus.java
@@ -1,4 +1,19 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class OrderStatus {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
 }
