919c0ec20c3539f2793de85273161388fe883d1a
myounghak <leemh9207@ajou.ac.kr>
2022-02-04 22:53:48 +0900

    add repository

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -25,4 +25,10 @@ public class MemberRepository {
         return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name =:name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
 }
