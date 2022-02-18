c797ca2ea09cc64136e8d6c2a608823cbcddee6f
myounghak <leemh9207@ajou.ac.kr>
2022-02-18 23:21:57 +0900

    오더생성

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -62,6 +62,26 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //생성 메서드//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member;
+        order.setDelivery(delivery);
+        for(OrderItem or : orderItems){
+            order.addOrderItem(or);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return oreder;
+
+    }
+
+    //비즈니스 로직
+    /*
+    주문 취소 
+     */
+
 
 
 }
