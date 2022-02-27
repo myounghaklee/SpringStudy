753c433e25b0fb1f92a3f0c05f7de981d578c84c
myounghak <leemh9207@ajou.ac.kr>
2022-02-27 20:45:15 +0900

    retrieve memberlist code add

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -39,7 +41,12 @@ public class MemberController {
          memberService.join(member);
          return "redirect:/";
     }
-
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.fimdMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 
 
 }
