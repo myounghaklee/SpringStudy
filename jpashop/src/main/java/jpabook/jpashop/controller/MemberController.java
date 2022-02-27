b49a6a376732a225139f7f4ec226b64eb9565c30
myounghak <leemh9207@ajou.ac.kr>
2022-02-27 13:19:55 +0900

    add 회원가입

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,11 +1,18 @@
 package jpabook.jpashop.controller;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -17,6 +24,22 @@ public class MemberController {
         return "members/createMemberForm";
 
     }
+
+    @PostMapping("members/new")
+    public String crate(@Valid MemberForm form, BindingResult result){
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+        log.info("post IN======");
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+         memberService.join(member);
+         return "redirect:/";
+    }
+
 
 
 }
