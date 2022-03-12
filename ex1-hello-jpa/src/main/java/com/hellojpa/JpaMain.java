da99a0ab944384e9c1ad8bd29a6a311c0c8767f6
myounghak <leemh9207@ajou.ac.kr>
2022-03-13 02:15:48 +0900

    connect to h2 && add test member

--- ex1-hello-jpa/src/main/java/com/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/com/hellojpa/JpaMain.java
@@ -2,13 +2,25 @@ package com.hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
+        //프로젝트당 하나만
         EntityManagerFactory emf= Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Member m = new Member();
+        m.setId(1L);
+        m.setName("hak");
+
+        em.persist(m);
+        tx.commit();
+
         em.close();
         emf.close();
 
