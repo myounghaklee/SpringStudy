c3502cd09a9df97c0c76ec0b99c8da8d6a9662ab
myounghak <leemh9207@ajou.ac.kr>
2022-03-21 01:28:52 +0900

    add order status

--- jpaBook/src/main/java/jpaBook/jpashop/domain/OrderStatus.java
+++ jpaBook/src/main/java/jpaBook/jpashop/domain/OrderStatus.java
@@ -1,17 +1,18 @@
 package jpaBook.jpashop.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-public class Order {
-
+public class OrderStatus {
     @Id
     @GeneratedValue
     private Long id;
     private Long memberId;
+
     private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
 }
