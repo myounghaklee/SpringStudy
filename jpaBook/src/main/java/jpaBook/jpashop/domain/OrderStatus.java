3a9e280e7b4f08f96583ee536798980696d28b40
myounghak <leemh9207@ajou.ac.kr>
2022-03-23 19:47:07 +0900

    orderstatus modify

--- jpaBook/src/main/java/jpaBook/jpashop/domain/OrderStatus.java
+++ jpaBook/src/main/java/jpaBook/jpashop/domain/OrderStatus.java
@@ -3,16 +3,6 @@ package jpaBook.jpashop.domain;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
-public class OrderStatus {
-    @Id
-    @GeneratedValue
-    private Long id;
-    private Long memberId;
-
-    private LocalDateTime orderDate;
-
-    @Enumerated(EnumType.STRING)
-    private OrderStatus status;
-
+public enum OrderStatus {
+    ORDER, CANCEL
 }
