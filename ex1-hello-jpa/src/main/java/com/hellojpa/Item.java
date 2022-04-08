712a64f1c7239fcf7e5bfc5592535e8d1644a6a4
myounghak <leemh9207@ajou.ac.kr>
2022-04-08 22:28:55 +0900

    item add

---
+++ ex1-hello-jpa/src/main/java/com/hellojpa/Item.java
@@ -0,0 +1,15 @@
+package com.hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+
+}
