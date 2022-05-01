c996ce6080818427ddc7f0adf06867f66efdb1ef
myounghak <leemh9207@ajou.ac.kr>
2022-05-01 23:11:03 +0900

    값타입

--- ex1-hello-jpa/src/main/java/com/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/com/hellojpa/Member.java
@@ -1,6 +1,7 @@
 package com.hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -13,55 +14,7 @@ public class Member  extends BaseEntity{
 
     @Column(name = "USERNAME")
     private String name;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
-    private Team team;
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    private int age;
-
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-        team.getMembers().add(this);
-    }
-
-    /* 생성자*/
-    public Member(){
-
-    }
-    //Getter Setter
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-
+    LocalDateTime startDate;
+    LocalDateTime endDate;
 }
