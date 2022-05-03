e4a264e864b0179ab47f084d2ef30f715339b36f
myounghak <leemh9207@ajou.ac.kr>
2022-05-03 20:31:39 +0900

    no message

--- ex1-hello-jpa/src/main/java/com/hellojpa/Address.java
+++ ex1-hello-jpa/src/main/java/com/hellojpa/Address.java
@@ -1,11 +1,13 @@
 package com.hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class Address {
     private String city;
     private String street;
+    @Column(name="ZIPCODE")
     private String zipcode;
 
     public Address() {
