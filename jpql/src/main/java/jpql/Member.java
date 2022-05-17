9844f515a6ba51a22efb5a7836fd93af05af5d13
myounghak <leemh9207@ajou.ac.kr>
2022-05-17 23:48:20 +0900

    Team, Member생성

---
+++ jpql/src/main/java/jpql/Member.java
@@ -0,0 +1,47 @@
+package jpql;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
---
+++ jpql/src/main/java/jpql/Team.java
@@ -0,0 +1,4 @@
+package jpql;
+
+public class Team {
+}
