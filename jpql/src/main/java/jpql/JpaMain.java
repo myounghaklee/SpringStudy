b4e29622cebbe2b396b6d5f749ecb4ad4050283a
myounghak <leemh9207@ajou.ac.kr>
2022-05-19 23:08:47 +0900

    add Entity to test jpql

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -7,6 +7,7 @@ import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,6 +19,15 @@ public class JpaMain {
         try{
            Member m = new Member();
            m.setUsername("member1");
+            String jpql = "select t from Team t join fetch t.members where t.name = '팀A'";
+            List<Team> teams = em.createQuery(jpql, Team.class).getResultList();
+            for(Team team : teams) {
+                System.out.println("teamname = " + team.getName() + ", team = " + team);
+                for (Member member : team.getMembers()) {
+                    //페치 조인으로 팀과 회원을 함께 조회해서 지연 로딩 발생 안함
+                    System.out.println("-> username = " + member.getUsername()+ ", member = " + member); }
+                }
+
            em.persist(m);
            tx.commit();
 
