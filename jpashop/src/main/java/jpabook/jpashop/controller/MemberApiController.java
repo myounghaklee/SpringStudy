4996d9584b60a9cdc1c960da130e62c0cea2fe10
myounghak <leemh9207@ajou.ac.kr>
2022-05-22 23:17:48 +0900

    회원가입 API controller add

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberApiController.java
@@ -1,13 +1,32 @@
 package jpabook.jpashop.controller;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Service.MemberService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
-    
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@ResponseBody @Valid Member member){
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+        public CreateMemberResponse(Long id){
+            this.id = id;
+        }
+    }
+
 }
