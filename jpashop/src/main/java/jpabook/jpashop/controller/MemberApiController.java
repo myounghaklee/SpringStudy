5923b73dd5c092aa1058bf59d0af2157110478fe
myounghak <leemh9207@ajou.ac.kr>
2022-05-25 19:59:48 +0900

    멤버조회 version2 add

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +20,28 @@ public class MemberApiController {
     @GetMapping("/api/vi/members")
     public List<Member> membersV1(){
         return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream()
+                .map(m-> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
     }
 
     @PostMapping("/api/v1/members")
