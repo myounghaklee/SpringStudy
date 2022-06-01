961fae90293b471384116cd396c723b3df72440b
myounghak <leemh9207@ajou.ac.kr>
2022-06-01 23:58:59 +0900

    no message

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+Order
+Order -> Memeber
+Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+}
