64b86ba3da27161f9c552f98a82c811e6cd1cec3
myounghak <leemh9207@ajou.ac.kr>
2022-06-02 23:29:54 +0900

    Entity노출 방법

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,7 +1,13 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /*
 Order
@@ -11,4 +17,11 @@ Order -> Delivery
 @RestController
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
 }
