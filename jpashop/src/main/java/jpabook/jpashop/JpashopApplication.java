8f9ce26d1375360e9ddf02e0e082a7e4d40a7bb9
myounghak <leemh9207@ajou.ac.kr>
2022-06-06 23:49:41 +0900

    Force_lazy는 지양

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -14,5 +16,12 @@ public class JpashopApplication {
 		System.out.println(data +"------");
 		SpringApplication.run(JpashopApplication.class, args);
 	}
+	@Bean
+	Hibernate5Module hibernate5Module(){
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
+	}
+
 
 }
