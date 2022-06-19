feb0deee025872bfb25aa7da1211def9058cce0b
myounghak <leemh9207@ajou.ac.kr>
2022-06-19 17:38:31 +0900

    dto생성중

--- jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -1,7 +1,18 @@
 package jpabook.jpashop.repository.order.query;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class OrderQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItemQueryDtos;
 }
