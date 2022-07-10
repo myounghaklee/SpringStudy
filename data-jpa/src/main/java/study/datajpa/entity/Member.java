5dcd78738f3db674c41e684824992de2220c3c6d
myounghak <leemh9207@ajou.ac.kr>
2022-07-10 17:40:41 +0900

    entity, repo 메서드 추가

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,4 +1,17 @@
 package study.datajpa.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter@Setter
 public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
 }
