cbfe6e8e45dae4d0df9ec5565d2d73d104701684
myounghak <leemh9207@ajou.ac.kr>
2022-07-17 11:18:39 +0900

    query interface add( named query, @query)

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,12 +1,19 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
- //   List<Member> findByUserNameAndAgeGreaterThan(String username, int age);
+    List<Member> findByUserNameAndAgeGreaterThan(String username, int age);
+    List<Member>findTop3HelloBy();
+    List<Member> findByusername(@Param("uwername") String username);
+
+    @Query("select m from Member m where m.username =: username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
 }
