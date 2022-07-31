b7196e87e3fac82d6b1ac2c9545a47319a94710f
myounghak <leemh9207@ajou.ac.kr>
2022-07-31 20:23:03 +0900

    bulk 쿼리 오타수정

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.Dto.MemberDto;
@@ -34,6 +35,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query(value = "select m from Member m left join m.team t",
             countQuery = "select count (m.username) from Member m")
     Page<Member> findByAge(int age, Pageable pageable);
+
+    @Modifying
+    @Query(value = "update Member m set m.age = m.age + 1 where m.age >= :age ")
+    Page<Member> findByAgeBulk(int age, Pageable pageable);
+
 
 }
 
