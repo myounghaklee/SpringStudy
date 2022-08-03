375a09b1d9865f3525c867d7551fa295741e2fed
myounghak <leemh9207@ajou.ac.kr>
2022-08-03 23:56:07 +0900

    ㅏ용자정의

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepositoryCustomImpl.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepositoryCustomImpl.java
@@ -1,4 +1,18 @@
 package study.datajpa.repository;
 
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class MemberRepositoryCustomImpl implements MemberRepositoryCustom{
+    private final  EntityManager em;
+
+    public MemberRepositoryCustomImpl(EntityManager em){
+        this.em = em;;
+    }
+    @Override
+    public List<Member> findMemberCustom() {
+        return null;
+    }
 }
