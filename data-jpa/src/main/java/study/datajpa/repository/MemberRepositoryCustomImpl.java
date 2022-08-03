9bb9a46abdfa6cc517f564ae3d4175ba014f1121
myounghak <leemh9207@ajou.ac.kr>
2022-08-03 23:56:51 +0900

    createquery추가

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepositoryCustomImpl.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepositoryCustomImpl.java
@@ -1,18 +1,17 @@
 package study.datajpa.repository;
 
+import lombok.RequiredArgsConstructor;
 import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
-public class MemberRepositoryCustomImpl implements MemberRepositoryCustom{
-    private final  EntityManager em;
+@RequiredArgsConstructor
+public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
+    private final EntityManager em;
 
-    public MemberRepositoryCustomImpl(EntityManager em){
-        this.em = em;;
-    }
     @Override
     public List<Member> findMemberCustom() {
-        return null;
+        return em.createQuery("select m from Member  m").getResultList();
     }
 }
