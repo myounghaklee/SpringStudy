79184e6476ef1ab596a4c639f7d4b873af05aa52
myounghak <leemh9207@ajou.ac.kr>
2022-08-09 19:14:03 +0900

    코드 포매팅

--- GoUpStudy/src/boj1158_요세푸스문제.java
+++ GoUpStudy/src/boj1158_요세푸스문제.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class boj1158_요세푸스문제 {
-    private static  int N;
+    private static int N;
     private static int K;
 
     public static void main(String[] args) {
@@ -10,20 +10,20 @@ public class boj1158_요세푸스문제 {
         K = sc.nextInt();
 
         Queue<Integer> dq = new LinkedList<>();
-        for(int i =0; i<N ;i++){
-            dq.add(i+1);
+        for (int i = 0; i < N; i++) {
+            dq.add(i + 1);
         }
         System.out.print("<");
-        while (!dq.isEmpty()){
-            for(int i =0; i<K ; i++){
-                if(i == K-1){
+        while (!dq.isEmpty()) {
+            for (int i = 0; i < K; i++) {
+                if (i == K - 1) {
                     int tmp = dq.remove();
-                    if(dq.size()==0){
+                    if (dq.size() == 0) {ㄴㄴㄴ
                         System.out.print(tmp);
-                    }else{
+                    } else {
                         System.out.print(tmp + ", ");
                     }
-                }else{
+                } else {
                     dq.add(dq.remove());
                 }
             }
