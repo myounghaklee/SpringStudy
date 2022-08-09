4dd0380621d5a0ad20de4909587c465846d0ce04
myounghak <leemh9207@ajou.ac.kr>
2022-08-09 19:14:26 +0900

    오타수정

--- GoUpStudy/src/boj1158_요세푸스문제.java
+++ GoUpStudy/src/boj1158_요세푸스문제.java
@@ -18,7 +18,7 @@ public class boj1158_요세푸스문제 {
             for (int i = 0; i < K; i++) {
                 if (i == K - 1) {
                     int tmp = dq.remove();
-                    if (dq.size() == 0) {ㄴㄴㄴ
+                    if (dq.size() == 0) {
                         System.out.print(tmp);
                     } else {
                         System.out.print(tmp + ", ");
