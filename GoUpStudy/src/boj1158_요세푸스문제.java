e90d23596e8dc7e876310ab4390aff6843070390
myounghak <leemh9207@ajou.ac.kr>
2022-08-09 19:13:26 +0900

    요세푸스

---
+++ GoUpStudy/src/boj1158_요세푸스문제.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class boj1158_요세푸스문제 {
+    private static  int N;
+    private static int K;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        Queue<Integer> dq = new LinkedList<>();
+        for(int i =0; i<N ;i++){
+            dq.add(i+1);
+        }
+        System.out.print("<");
+        while (!dq.isEmpty()){
+            for(int i =0; i<K ; i++){
+                if(i == K-1){
+                    int tmp = dq.remove();
+                    if(dq.size()==0){
+                        System.out.print(tmp);
+                    }else{
+                        System.out.print(tmp + ", ");
+                    }
+                }else{
+                    dq.add(dq.remove());
+                }
+            }
+        }
+        System.out.print(">");
+    }
+}
--- data-jpa/src/main/java/study/datajpa/entity/BaseEntity.java
+++ data-jpa/src/main/java/study/datajpa/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package study.datajpa.entity;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -20,4 +21,12 @@ public class BaseEntity {
 
     @LastModifiedBy
     private LocalDateTime lastModifiedDate;
+
+    //등록자 수정자
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
