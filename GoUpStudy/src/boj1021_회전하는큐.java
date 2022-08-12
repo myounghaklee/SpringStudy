02048aa592be01ad91a6e8939c2179428ec7d6e7
myounghak <leemh9207@ajou.ac.kr>
2022-08-12 19:26:18 +0900

    회전하는큐 add

---
+++ GoUpStudy/src/boj1021_회전하는큐.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class boj1021_회전하는큐 {
+    private static int N;
+    private static int M;
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        N = Integer.parseInt(st.nextToken());
+//        M = Integer.parseInt(st.nextToken());
+//
+//        st = new StringTokenizer(br.readLine(), " ");
+//        Deque<Integer> dq = new LinkedList<>();
+//        int tmp =1;
+//        //dq.stream().forEach(i -> dq.add(i+1));
+
+ //       System.out.println(dq.toString());
+
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        int a = list.get(0);
+        Integer tmp = a;
+        Integer tmp2 = a;
+        System.out.println(a);
+//        ArrayList<Integer> lista = new ArrayList<>();
+//        Scanner sc = new Scanner(System.in);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+
+    }
+
+}
