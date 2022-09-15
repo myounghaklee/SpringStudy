90b20ae50b39aee0fecef038ea54143fb2f52f14
myounghaklee <leemh9207@ajou.ac.kr>
2022-09-15 15:18:52 +0900

    querydsl-jpa 테스트 추가

--- querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -3,10 +3,13 @@ package study.querydsl.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
+
+import static study.querydsl.entity.QMember.member;
 
 @Repository
 public class MemberJpaRepository {
@@ -32,10 +35,20 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
-
+    public List<Member> findAll_QueryDsl(){
+        return queryFactory.selectFrom(member).fetch();
+    }
     public List<Member> findByUsername(String username){
         return em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", username)
                 .getResultList();
     }
+
+    public List<Member> findByUsername_QueryDsl(String username){
+        return queryFactory.select(member)
+                .from(member)
+                .where(member.username.eq(username))
+                .fetch();
+    }
+
 }
