17aca2f2e00618d631cd85fc1f84c2be7234abb4
myounghak <leemh9207@ajou.ac.kr>
2022-09-20 00:02:06 +0900

    initmember service 의존성 주입

--- querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+
 @Transactional
 class MemberJpaRepositoryTest {
     @Autowired
