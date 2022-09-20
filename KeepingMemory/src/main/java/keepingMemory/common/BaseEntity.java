cc1825300ea1f64acfab57c0652bc045bdd8455b
myounghaklee <leemh9207@ajou.ac.kr>
2022-09-20 14:19:26 +0900

    user entity 생성

---
+++ KeepingMemory/src/main/java/keepingMemory/common/BaseEntity.java
@@ -0,0 +1,39 @@
+package keepingMemory.common;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private ZonedDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private ZonedDateTime updatedAt;
+
+    @Setter
+    @CreatedBy
+    @Column(name = "creator_id", nullable = false, updatable = false)
+    private Long creatorId = 1L;
+
+    @LastModifiedBy
+    @Column(name = "updater_id", nullable = false)
+    private Long updaterId = 1L;
+
+}
