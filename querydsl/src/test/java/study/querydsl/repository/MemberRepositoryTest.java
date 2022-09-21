58dca8388bc1eb3917a20398f27933170fcca7f8
myounghaklee <leemh9207@ajou.ac.kr>
2022-09-21 10:09:37 +0900

    테스트 코드 추가

--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,12 +1,17 @@
 package study.querydsl.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -20,7 +25,17 @@ class MemberRepositoryTest {
 
     @Test
     void basicTest(){
-        
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
     }
 
 }
