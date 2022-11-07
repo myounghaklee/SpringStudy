ae7ac3fa88ba00d7e086782d39e022e277a1dde4
devhak <devhak.enzo@gmail.com>
2022-11-07 13:53:43 +0900

    완전탐색 풀이 완료 - 크기순 배열 정렬 - 가로 세로 완탐

--- GoUpStudy/src/코드/프로그래머스/최소직사각형_완탐.java
+++ GoUpStudy/src/코드/프로그래머스/최소직사각형_완탐.java
@@ -3,19 +3,33 @@ package 코드.프로그래머스;
 public class 최소직사각형_완탐 {
     public static void main(String[] args) {
 
-        int sizes [][] = {{60,50}, {30,70}, {60,30}, {80,40}};
+        int sizes[][] = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
 
         int ans = solution(sizes);
+        System.out.println(ans);
+
     }
 
 
     public static int solution(int[][] sizes) {
         int seroMax = Integer.MIN_VALUE;
         int garoMax = Integer.MIN_VALUE;
-        for(int i =0; i< sizes.length; i++){
-            seroMax = sizes[i][0]>seroMax ? sizes[i][0]: seroMax;
+        for (int i = 0; i < sizes.length; i++) {
+            if(sizes[i][0] < sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+
         }
-        int answer = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            seroMax = sizes[i][0] > seroMax ? sizes[i][0] : seroMax;
+        }
+        for (int i = 0; i < sizes.length; i++) {
+            garoMax = sizes[i][1] > garoMax ? sizes[i][1] : garoMax;
+        }
+
+        int answer = garoMax * seroMax;
         return answer;
     }
 }
