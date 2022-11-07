0eddf37353e2efb48b30896e9c05f2c5a0bed4ee
devhak <devhak.enzo@gmail.com>
2022-11-07 17:58:55 +0900

    포맷 add

---
+++ GoUpStudy/src/코드/프로그래머스/모의고사_완탐.java
@@ -0,0 +1,12 @@
+package 코드.프로그래머스;
+
+public class 모의고사_완탐 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+}
